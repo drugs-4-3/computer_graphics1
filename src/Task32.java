a991bab0c8e02e2b8400ddd85dc994ceb89684d5
michal.sciubidlo <michal.sciubidlo@limango.pl>
2017-10-15 13:13:56 +0200

    chessboard2 done

--- src/Task32.java
+++ src/Task32.java
@@ -11,32 +11,45 @@ public class Task32 {
 
         System.out.println("Chessboard pattern synthesis 2");
 
-        BufferedImage image;
-
+        // arguments from console
         final int x_res = Integer.parseInt(args[0].trim());
         final int y_res = Integer.parseInt(args[1].trim());
         final int field_width = Integer.parseInt(args[2].trim());
+        final String filename = args[3];
+
+
         final Color color1 = new Color(0,0,0);
         final Color color2 = new Color(255,255,255);
-        final String filename = args[3];
         final double a = field_width*Math.sqrt(2);
 
-        image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
-
+        BufferedImage image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
+        Color color;
         for (int i = 0; i < y_res; i++) {
             for (int j = 0; j < x_res; j++) {
-                double dj = j % a;
-                double di = i % a;
-                if (di < a/2) {
-                    if (dj < a/2) {
-                        if (i )
+                int ni = (int)(i % a);
+                int nj = (int)(j % a);
+                if (nj < (a/2)) {
+                    if (ni < (a/2)) {
+                        color = (ni + nj) < (a/2) ? color1 : color2;
+                    }
+                    else {
+                        ni = (int)(ni % (a/2));
+                        color = (ni % (a/2)) - nj >= 0 ? color1 : color2;
                     }
                 }
                 else {
-
+                    nj = (int)(nj % (a/2));
+                    if (ni < (a/2)) {
+                        color = ni - nj < 0 ? color1 : color2;
+                    } else {
+                        ni = (int)(ni % (a/2));
+                        color = ni + nj >= a/2 ? color1 : color2;
+                    }
                 }
+                image.setRGB(j, i, color.getRGB());
             }
         }
+        System.out.println("We did it!");
 
         try {
             ImageIO.write(image, "BMP", new File(filename));
