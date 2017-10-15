026f960d58ea3dff822e5334acbab882b6d920a2
michal.sciubidlo <michal.sciubidlo@limango.pl>
2017-10-15 22:05:34 +0200

    Task2 modified

--- src/Task2.java
+++ src/Task2.java
@@ -5,11 +5,6 @@ import javax.imageio.*;
 
 public class Task2 {
 
-
-    // todo: dodać parametry wyspecyfikowane w poleceniu
-    // todo: wprowadzić ograniczenie na parametry tak, że nie da się kraty szerszej niż odległość od środków itp.
-
-
     public static void main(String[] args) {
 
         System.out.println("Grid pattern synthesis");
@@ -19,17 +14,17 @@ public class Task2 {
         final int x_res = Integer.parseInt(args[0].trim());
         final int y_res = Integer.parseInt(args[1].trim());
         final int bar_width = Integer.parseInt(args[2].trim());
-        final int center_distance = Integer.parseInt(args[3].trim());
-        final String filename = args[4];
-        final int square_width = center_distance - bar_width;
+        final int center_distance_x = Integer.parseInt(args[3].trim());
+        final int center_distance_y = Integer.parseInt(args[4].trim());
+        final String filename = args[5];
 
         image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
 
         for (int i = 0; i < y_res; i++) {
             for (int j = 0; j < x_res; j++) {
                 Color color = new Color(0,0,0);
-                if (j % center_distance < square_width/2 || j % center_distance > square_width/2 + bar_width) {
-                    if (i % center_distance < square_width/2 || i % center_distance > square_width/2 + bar_width) {
+                if (j % center_distance_x < (center_distance_x - bar_width)/2 || j % center_distance_x > (center_distance_x - bar_width)/2 + bar_width) {
+                    if (i % center_distance_y < (center_distance_y - bar_width)/2 || i % center_distance_y > (center_distance_y - bar_width)/2 + bar_width) {
                         color = new Color(255,255,255);
                     }
                 }
